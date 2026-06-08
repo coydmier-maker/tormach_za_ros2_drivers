0171f06930c9b7919ed37b1983776c839052c519
coydmier-maker <coydmier@gmail.com>
2026-06-08 15:08:10 -0500

    despacito

--- scripts/cartesian_motion_test.py
+++ scripts/cartesian_motion_test.py
@@ -40,7 +40,8 @@ def main():
         
         time.sleep(2.0)
 
-        robot.set_speed(0.0025)
+        robot.set_speed(0.000025)
+        time.sleep(1)
 
         robot.move_to_named_pose("preparing", cartesian=True)
         time.sleep(1.0)
