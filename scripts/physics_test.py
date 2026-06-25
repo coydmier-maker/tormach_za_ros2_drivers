4f01566c12b0446c20f2707fa4136338f4506a35
coydmier-maker <coydmier@gmail.com>
2026-06-25 15:42:47 -0400

    full motion test

--- scripts/physics_test.py
+++ scripts/physics_test.py
@@ -39,9 +39,17 @@ def main():
 
         scene_path = Path(__file__).parent.parent / "python_api" / "config" / "scene.yaml"
         robot.scene.load_yaml(str(scene_path))
+        robot.scene.add_gripper_collider()
         time.sleep(2.0)
 
         robot.set_speed(0.025)
+
+        # -----------------------------------
+        # Start movement
+        # -----------------------------------
+
+        # Ungrip
+        robot.gripper.open()
 
         # Ready
         robot.move_joints(
@@ -50,11 +58,48 @@ def main():
 
         # Apprach
         robot.move_joints(
-            joint_positions=[0.0156, 1.3746, 0.482, 0.332, 0.016, -1.626],
+            joint_positions=[0.0156, 1.3746, 0.482, 0.332, 0.016, -1.626]
         )
 
         # Grip
         robot.gripper.close()
+
+        # Move up
+        robot.move_joints(
+            joint_positions=[0.015, 1.082, 0.480, 0.487, 0.017, -1.487]
+        )
+
+        # Move over
+        robot.move_joints(
+            joint_positions=[-0.410, 1.078, 0.528, 0.375, -0.415, -1.380]
+        )
+
+        # Go down
+        robot.move_joints(
+            joint_positions=[-0.407, 1.476, 0.473, 0.112, -0.404, -1.505]
+        )
+
+        # Push off
+        robot.move_joints(
+            joint_positions=[0.581, 1.500, 0.401, -0.070, 0.594, -1.372]
+        )
+
+        # Back to middle
+        robot.move_joints(
+            joint_positions=[-0.018, 1.243, 0.480, 0.342, -0.016, -1.502]
+        )
+
+        # Drop
+        robot.gripper.open()
+
+        # Return home
+        robot.move_joints(
+            joint_positions=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        )
+
+        # -----------------------------------
+        # End movement
+        # -----------------------------------
 
         time.sleep(2.0)
 
